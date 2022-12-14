50ba76b105814ecdfa5c6a52f293a556f8557548
Adam Featherston <j.adam.featherston@gmail.com>
2022-12-14 15:02:41 -0600

    Clothing Item List and Managers.  Nav Bars and views

---
+++ src/managers/ClothingItemManager.js
@@ -0,0 +1,72 @@
+export const getGames = () => {
+    return fetch("http://localhost:8000/games", {
+        method: "GET",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+}
+
+export const getSingleGame = (id) => {
+    return fetch(`http://localhost:8000/games/${id}`, {
+        method: "GET",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+}
+
+export const createGame = (game) => {
+    return fetch("http://localhost:8000/games", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        },
+        body: JSON.stringify(game)
+    })
+        .then(response => response.json())
+}
+
+export const getGameTypes = () => {
+    return fetch("http://localhost:8000/gametypes", {
+        method: "GET",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+}
+
+export const updateGameDetails = (game) => {
+    return fetch(`http://localhost:8000/games/${game.id}`, {
+        method: "PUT",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        },
+        body: JSON.stringify(game)
+    })
+        //.then(response => response.json()) is not used when returning None and a status code on the server side.
+}
+
+export const deleteGame = (id) => {
+    return fetch(`http://localhost:8000/games/${id}`, {
+        method: "DELETE",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+        })
+}
